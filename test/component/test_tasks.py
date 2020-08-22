89ef3ebf2e911990dd8da428574dcc5743ac8fc8
Tom Noble <tom.noble@bjss.com>
2020-08-22 16:19:13 +0100

    Remove file content checking from component tests, as this is covered in unit tests

--- test/component/test_tasks.py
+++ test/component/test_tasks.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 import jmespath
 import pytest
-import pandas as pd
 import boto3
 import botocore.exceptions
 
@@ -37,13 +36,13 @@ def mock_files():
     source_bucket_name = get_stack_output("SourceBucketName")
     processing_bucket_name = get_stack_output("ProcessingBucketName")
     destination_bucket_name = get_stack_output("DestinationBucketName")
-    s3_client.put_object(Bucket=source_bucket_name, Key="test_files/file1", Body="")
-    s3_client.put_object(Bucket=source_bucket_name, Key="test_files/file2", Body="")
-    s3_client.upload_file(Bucket=source_bucket_name, Key="test_files/people.csv", Filename=str(DATA_DIR/"people.csv"))
-    s3_client.upload_file(Bucket=source_bucket_name, Key="test_files/jobs.csv", Filename=str(DATA_DIR/"jobs.csv"))
-    s3_client.upload_file(Bucket=processing_bucket_name, Key="test_files/people_string_replaced.csv", Filename=str(DATA_DIR/"people_string_replaced.csv"))
-    s3_client.upload_file(Bucket=processing_bucket_name, Key="test_files/jobs_string_replaced.csv", Filename=str(DATA_DIR / "jobs_string_replaced.csv"))
-    s3_client.upload_file(Bucket=processing_bucket_name, Key="test_files/merged.csv", Filename=str(DATA_DIR/"merged.csv"))
+    s3_client.put_object(Bucket=source_bucket_name, Key="test_input/file1", Body="")
+    s3_client.put_object(Bucket=source_bucket_name, Key="test_input/file2", Body="")
+    s3_client.upload_file(Bucket=source_bucket_name, Key="test_input/people.csv", Filename=str(DATA_DIR/"people.csv"))
+    s3_client.upload_file(Bucket=source_bucket_name, Key="test_input/jobs.csv", Filename=str(DATA_DIR/"jobs.csv"))
+    s3_client.upload_file(Bucket=processing_bucket_name, Key="test_input/people.csv", Filename=str(DATA_DIR/"people.csv"))
+    s3_client.upload_file(Bucket=processing_bucket_name, Key="test_input/jobs.csv", Filename=str(DATA_DIR/"jobs.csv"))
+    s3_client.upload_file(Bucket=processing_bucket_name, Key="test_input/merged.csv", Filename=str(DATA_DIR/"merged.csv"))
     yield
     s3_resource.Bucket(source_bucket_name).objects.all().delete()
     s3_resource.Bucket(processing_bucket_name).objects.all().delete()
@@ -54,11 +53,11 @@ def mock_files():
     "required_files, expected_result",
     [
         ([], True),
-        (["test_files/file1"], True),
-        (["test_files/file3"], False),
-        (["test_files/file1", "test_files/file2"], True),
-        (["test_files/file1", "test_files/file3"], False),
-        (["test_files/file1", "test_files/file2", "test_files/file3"], False)
+        (["test_input/file1"], True),
+        (["test_input/file3"], False),
+        (["test_input/file1", "test_input/file2"], True),
+        (["test_input/file1", "test_input/file3"], False),
+        (["test_input/file1", "test_input/file2", "test_input/file3"], False)
     ]
 )
 def test_check_workflow_ready_lambda(mock_files, required_files, expected_result):
@@ -74,17 +73,12 @@ def test_check_workflow_ready_lambda(mock_files, required_files, expected_result
 
 
 @pytest.mark.parametrize(
-    "input_key, expected_file",
-    [
-        ("test_files/people.csv", "people_string_replaced.csv"),
-        ("test_files/jobs.csv", "jobs_string_replaced.csv")
-    ]
+    "input_key", ["test_input/people.csv", "test_input/jobs.csv"]
 )
-def test_string_replace_lambda(mock_files, input_key, expected_file):
-    s3_client = boto3.client("s3")
+def test_string_replace_lambda(mock_files, input_key):
     lambda_client = boto3.client("lambda")
     output_bucket = get_stack_output("ProcessingBucketName")
-    output_key = f"test_files/{input_key}"
+    output_key = f"test_output/{input_key}"
     event = {
         "InputBucket": get_stack_output("SourceBucketName"),
         "InputKey": input_key,
@@ -97,20 +91,16 @@ def test_string_replace_lambda(mock_files, input_key, expected_file):
     response = lambda_client.invoke(FunctionName=function_name, Payload=json.dumps(event))
     assert response["StatusCode"] == 200
     assert object_exists(bucket=output_bucket, key=output_key)
-    expected = pd.read_csv(DATA_DIR/expected_file)
-    actual = pd.read_csv(s3_client.get_object(Bucket=output_bucket, Key=output_key)["Body"])
-    pd.testing.assert_frame_equal(actual, expected)
 
 
 def test_calculate_total_earnings_lambda(mock_files):
-    s3_client = boto3.client("s3")
     lambda_client = boto3.client("lambda")
     output_bucket = get_stack_output("ProcessingBucketName")
-    output_key = "test_files/merged.csv"
+    output_key = "test_output/merged.csv"
     event = {
         "InputBucket": get_stack_output("ProcessingBucketName"),
-        "PeopleKey": "test_files/people.csv",
-        "JobsKey": "test_files/jobs.csv",
+        "PeopleKey": "test_input/people.csv",
+        "JobsKey": "test_input/jobs.csv",
         "OutputBucket": output_bucket,
         "OutputKey": output_key
     }
@@ -118,19 +108,15 @@ def test_calculate_total_earnings_lambda(mock_files):
     response = lambda_client.invoke(FunctionName=function_name, Payload=json.dumps(event))
     assert response["StatusCode"] == 200
     assert object_exists(bucket=event["OutputBucket"], key=event["OutputKey"])
-    expected = pd.read_csv(DATA_DIR/"merged.csv")
-    actual = pd.read_csv(s3_client.get_object(Bucket=output_bucket, Key=output_key)["Body"])
-    pd.testing.assert_frame_equal(actual, expected)
 
 
 def test_convert_csv_to_json_lambda(mock_files):
-    s3_client = boto3.client("s3")
     lambda_client = boto3.client("lambda")
     output_bucket = get_stack_output("DestinationBucketName")
-    output_key = "test_files/output.json"
+    output_key = "test_output/output.json"
     event = {
         "InputBucket": get_stack_output("ProcessingBucketName"),
-        "InputKey": "test_files/merged.csv",
+        "InputKey": "test_input/merged.csv",
         "OutputBucket": output_bucket,
         "OutputKey": output_key
     }
@@ -138,8 +124,5 @@ def test_convert_csv_to_json_lambda(mock_files):
     response = lambda_client.invoke(FunctionName=function_name, Payload=json.dumps(event))
     assert response["StatusCode"] == 200
     assert object_exists(bucket=event["OutputBucket"], key=event["OutputKey"])
-    with open(DATA_DIR/"output.json", "r") as expected_file:
-        expected = json.load(expected_file)
-    actual = json.loads(s3_client.get_object(Bucket=output_bucket, Key=output_key)["Body"].read())
-    assert actual == expected
+
 
